0666b103d74ea62fb429728881139818696d2653
Blockchain Dev <nevillebrown_930@hotmail.com>
2018-10-11 18:23:54 -0400

    Update

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 create DATABASE burgers_db;
-For development;
+
 USE burgers_db;
-FROM https://burgeration.herokuapp.com/ 
 
 
-CREATE TABLE burgers
-(
+
+CREATE TABLE burgers(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   burger_name varchar(125) NOT NULL,
-  devoured BOOLEAN DEFAULT false
+  devoured BOOLEAN DEFAULT false)
